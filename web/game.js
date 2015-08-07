e8a86d3ae38295014763c40d312eb1e45de637c4
a0135523 <a0135523@u.nus.edu>
2015-08-07 11:19:59 +0800

    update on game html

--- web/game.js
+++ web/game.js
@@ -1,46 +1,43 @@
 var playerlisttemplate =
         Handlebars.compile($("#playerlisttemplate").html());
 
+var ipaddr = "";
+
 var gameid = 1;//get from session
 
 chosencards = new Array();
 
 $(document).ready(function () {
-    //This is used to size the the card squareness
+    //This is used to size up the Grid Square
     $("#selectable li").css("height", $("#selectable li").css("width"));
 
-    //passed session gameid into var gameid, to get the game object and then append accordingly to each field including the handlebar
-        $.getJSON(ipaddr + "api/game/" + gameid)
-                .done(function (result) {
-                    $("#tb_gametitle").val(result.title);
-                    $("#tb_timestarted").val(result.startTime);
-                    $("#tb_timeremaining").val();
-                    $("#tb_remainingcards").val(result.deck.length);
-                    $("#tb_noofplayers").val(result.playerScoreArray.length);
+    $(function () {
+        $("#selectable").bind("mousedown", function (e) {
+            e.metaKey = true;
+        }).selectable({
+            stop: function () {
+                $(".ui-selected", this).each(function () {
+                    chosencards.push($(this).attr("id"));
                 });
+            }
+        });
     });
 
-
-
-
-$(function () {
-
-    $("#selectable").bind("mousedown", function (e) {
-        e.metaKey = true;
-    }).selectable({
-        stop: function () {
-            $(".ui-selected", this).each(function () {
-                chosencards.push($(this).attr("id"));
+    //passed session gameid into var gameid, to get the game object and then append accordingly to each field including the handlebar
+    $.getJSON(ipaddr + "api/game/" + gameid)
+            .done(function (result) {
+                $("#tb_gametitle").val(result.title);
+                $("#tb_timestarted").val(result.startTime);
+                $("#tb_timeremaining").val();
+                $("#tb_remainingcards").val(result.deck.length);
+                $("#tb_noofplayers").val(result.playerScoreArray.length);
             });
-        }
-    });
 });
 
 $("#gamesubmit").click(function ()
 {
     alert(chosencards.toString());
 });
-
 
 //YOUTUBE VIDEO
 var youtubeFunc = '';
@@ -54,5 +51,3 @@ $('#introVideo').on('shown.bs.modal', function (e) {
     youtubeFunc = 'playVideo';
     youtubeIframe.postMessage('{"event":"command","func":"' + youtubeFunc + '","args":""}', '*');
 });
-
- 
