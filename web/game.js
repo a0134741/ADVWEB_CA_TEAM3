def4e691fac7fe23598320b18f8e10edff8a902c
a0134741 <a0134741@u.nus.edu>
2015-08-07 14:08:20 +0800

    Get 3 cards

--- web/game.js
+++ web/game.js
@@ -5,7 +5,10 @@ var ipaddr = "";
 
 var gameid = 1;//get from session
 
+var choose = 0;
 
+var position = new Array();
+var i=0;
 
 chosencards = new Array();
 
@@ -13,21 +16,56 @@ $(document).ready(function () {
     //This is used to size up the Grid Square
     $("#selectable li").css("height", $("#selectable li").css("width"));
 
-    $(function () {
-        $("#selectable").bind("mousedown", function (e) {
-            e.metaKey = true;
-        }).selectable({
-            stop: function () {
-                $(".ui-selected", this).each(function () {
-                    chosencards.push($(this).attr("id"));
-                });
+//    $(function () {
+//        $("#selectable").bind("mousedown", function (e) {
+//            e.metaKey = true;
+//        }).selectable({
+//            stop: function () {
+//                $(".ui-selected", this).each(function () {
+//                    chosencards.push($(this).attr("id"));
+//                });
+//            }
+//        });
+//    });
+
+    $("#selectable li").on("click", function () {
+        if (choose < 3)
+        {
+            if ($(this).hasClass("selected")) {
+                $(this).removeClass("selected");
+                choose = choose - 1;
             }
-        });
-    });
+            else {
+                $(this).addClass("selected");
+                choose = choose + 1;
+            }
+        }
+        else {
+            if ($(this).hasClass("selected")) {
+                $(this).removeClass("selected");
+                choose = choose - 1;
+            }
+            else {
+                alert(">3");
+            }
+        }
+    })
 
     $("#gamesubmit").click(function ()
     {
-        alert(chosencards.toString());
+
+        if (choose = 3) {
+            $("#selectable li").each(function () {
+                if ($(this).hasClass("selected")) {
+                    position[i]=$(this).attr("id");
+                    i++;
+                }
+            });
+            alert(position);
+        }
+        else {
+            alert("No enough 3 cards");
+        }
     });
 
     //passed session gameid into var gameid, to get the game object and then append accordingly to each field including the handlebar
@@ -38,13 +76,13 @@ $(document).ready(function () {
                 $("#tb_timeremaining").val();
                 $("#tb_remainingcards").val(result.deck.length);
                 $("#tb_noofplayers").val(result.playerScoreArray.length);
-                
+
                 var players = result.playerScoreArray;
                 for (var i in players) {
                     var gravatarcode = $.md5(players[i].player.email);
                     $("#table_player").append(
                             playerlisttemplate({
-                                gravatarurl: "https://s.gravatar.com/avatar/"+gravatarcode,
+                                gravatarurl: "https://s.gravatar.com/avatar/" + gravatarcode,
                                 playername: players[i].player.name,
                                 playerscore: players[i].currentScore
                             })
--- web/index.js
+++ web/index.js
@@ -61,53 +61,6 @@ $(document).ready(function () {
             window.location.href = "game.html";
         }
     });
-
-
-
-
-//    $('#gameform')
-//            .formValidation({
-//                framework: 'bootstrap',
-//                icon: {
-//                    valid: 'glyphicon glyphicon-ok',
-//                    invalid: 'glyphicon glyphicon-remove',
-//                    validating: 'glyphicon glyphicon-refresh'
-//                },
-//                excluded: ':disabled',
-//                fields: {
-//                    title: {
-//                        validators: {
-//                            notEmpty: {
-//                                message: 'The name is required'
-//                            },
-//                            stringLength: {
-//                                min: 6,
-//                                max: 30,
-//                                message: 'The name must be more than 6 and less than 30 characters long'
-//                            }
-//                        }
-//                    },
-//                    players: {
-//                        validators: {
-//                            notEmpty: {
-//                                message: 'The size is required'
-//                            }
-//                        }
-//                    },
-//                    duration: {
-//                        validators: {
-//                            notEmpty: {
-//                                message: 'The color is required'
-//                            }
-//                        }
-//                    }
-//                }
-//            })
-//            // Using Bootbox for color and size select elements
-//            .find('[name="color"], [name="size"]')
-//            .combobox()
-//            .end();
-
 });
 function getgamedetail(gameid) {
     //alert(gameid);
@@ -131,8 +84,7 @@ function getgamedetail(gameid) {
                             );
                 }
             });
-}
-;
+};
 
 
 function getallgame() {
